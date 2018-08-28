d76f099f339a357d7909bb50d37b4dd94af09dd0
Gary Muir <garymuir17@gmail.com>
2018-08-28 15:19:50 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+// import chart container
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <h1>Testing</h1>
+    );
+  }
+}
+
+export default App;
