82eaf95f4b1c8fbbe38c454a387e7997ccd6468b
Gary Muir <garymuir17@gmail.com>
2018-08-28 15:45:50 +0100

    adds options to the drop down menu

--- src/components/SongSelector.js
+++ src/components/SongSelector.js
@@ -1,12 +1,21 @@
 import React from 'react';
 
-const SongSelector = () => {
+const SongSelector = (props) => {
+  if (!props.songs.feed) return null;
+  console.log(props.songs.feed.entry[0]["im:name"].label);
+
+  const options = props.songs.feed.entry.map((song, index) => {
+    return <option value={index} key={index}>{song["im:name"].label}</option>
+  })
+
+
   return (
     <select
       id="song-selector"
       defaultValue="default"
     >
       <option disabled value="default">Pick a Song</option>
+      {options}
     </select>
   )
 }
--- src/containers/ChartContainer.js
+++ src/containers/ChartContainer.js
@@ -11,15 +11,31 @@ class ChartContainer extends React.Component {
     };
   };
 
+  componentDidMount() {
+    const url = 'https://itunes.apple.com/gb/rss/topsongs/limit=20/json';
+    fetch(url)
+      .then((res) => {
+        return res.json();
+      })
+      .then((songs) => {
+        this.setState({ songs: songs });
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
   render() {
     return (
       <div>
         <h1>Itunes Chart</h1>
-        <SongSelector />
+        <SongSelector
+          songs={this.state.songs}
+        />
         <SongDetail />
       </div>
     )
-  }
-}
+  };
+};
 
 export default ChartContainer;
