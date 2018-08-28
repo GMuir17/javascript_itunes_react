01b1239475e6af5fdb551096fdb8894f3351acb6
Gary Muir <garymuir17@gmail.com>
2018-08-28 16:15:11 +0100

    adds title and artist name to song detail

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -3,13 +3,14 @@ import React from 'react';
 const SongDetail = (props) => {
   if (!props.selectedSong || !props.selectedSongIndex) return null;
   console.log("SONG: ", props.selectedSong);
-  console.log("INDEX ", props.selectedSongIndex);
 
   const chartPosition = parseInt(props.selectedSongIndex, 10) + 1;
 
   return (
     <div>
       <h3>At Position {chartPosition}</h3>
+      <h4>Title - {props.selectedSong["im:name"].label}</h4>
+      <h4>Artist - {props.selectedSong["im:artist"].label}</h4>
     </div>
   );
 }
